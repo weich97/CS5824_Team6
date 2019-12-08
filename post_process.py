111d59330038e04f573042180e66dd6cd69c3b74
Ricky Tan <rickytanrmst@gmail.com>
2019-12-08 17:42:27 -0500

    modified post_process, added 1st div result

--- post_process.py
+++ post_process.py
@@ -1,23 +1,34 @@
+"""
+Author:     Richard Tan (rickytan@vt.edu)
+Version:    2019.12.08
+"""
+
+# ------------------------------------------------------------------------- Imports
 import numpy as np
 import matplotlib.pyplot as plt
 
+# ------------------------------------------------------------------------- Global Variables
+DATA_FILE = ".\data\lr_0005__lambda_02.npz"
 
-DATA_FILE = "Serial-mesh32-convalue0.npz"
-
+# load the specified file
 data = np.load(DATA_FILE)
 train_hist = data['a'][()]
 
-# Divergence
+# ------------------------------------------------------------------------- Divergence
 dr = train_hist['delta_real']
 dl = train_hist['delta_lose']
 
+# plot divergence
 plt.plot(range(len(dr)),dr,'r--',range(len(dl)),dl,'bs')
+plt.yscale('log')
+plt.ylabel('mean divergence')
+plt.xlabel('epoch')
 plt.show()
 
 
-# Contours
-x = train_hist['validate'][21] # (1,32,32,3)
-print(x.shape)
-fig, ax = plt.subplots()
-ax.contour(x[0,:,:,0])
-plt.show()
+# ------------------------------------------------------------------------- Contours
+# x = train_hist['validate'][0] # (1,32,32,3)
+# print(x.shape)
+# fig, ax = plt.subplots()
+# ax.contour(x[0,:,:,0])
+# plt.show()
