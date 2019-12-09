e58db709f6d9d3b3ccb619d4a19bdad37588dd49
Ricky Tan <rickytanrmst@gmail.com>
2019-12-08 20:09:53 -0500

    got basic streamplot

--- post_process.py
+++ post_process.py
@@ -11,9 +11,10 @@ import matplotlib.pyplot as plt
 DATA_DIR = ".\data"
 LABELS = ["lr=0.0005, lambda=0.2", "lr=0.005, lambda=2", "lr=0.005, lambda=0.2"]
 LABELS_SINGULARITY = ["removed 9.38% points around singularity", "removed 21.8% points around singularity"]
+LABELS_CONTOURS = ["training", ""]
 
 # ------------------------------------------------------------------------- Divergence
-
+"""
 for filename in os.listdir(DATA_DIR):
     if filename.endswith(".npz"):
         filepath = os.path.join(DATA_DIR, filename)
@@ -33,10 +34,35 @@ plt.yscale('log')
 plt.ylabel('mean divergence')
 plt.xlabel('epoch')
 plt.show()
+"""
 
 # ------------------------------------------------------------------------- Contours
+
+for filename in os.listdir(DATA_DIR):
+    if filename.endswith(".npz"):
+        filepath = os.path.join(DATA_DIR, filename)
+        print("loading %s"%filename)
+
+        # load contours
+        data = np.load(filepath)
+        train_hist = data['a'][()]
+
+        # set up data
+        training = train_hist['validate'][0]
+        x = np.linspace(-16,16,32)
+        y = np.linspace(-16,16,32)
+        u = training[0,:,:,0]
+        v = training[0,:,:,1]
+        speed = np.sqrt(u*u + v*v)
+
+        # plot data
+        plt.streamplot(x,y,u,v,color=speed)
+        plt.show()
+        
+
 # x = train_hist['validate'][0] # (1,32,32,3)
-# print(x.shape)
-# fig, ax = plt.subplots()
-# ax.contour(x[0,:,:,0])
-# plt.show()
+"""
+fig, ax = plt.subplots()
+ax.contour(x[0,:,:,0])
+plt.show()
+"""
